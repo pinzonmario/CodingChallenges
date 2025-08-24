eaa02ba5c72ebfa22ff7e430f202132e5ab71942
Mario Pinzón <mariopinzon.dev@gmail.com>
2025-08-24 12:00:19 -0500

    exercise(hackerrank): solve "Simple Array Sum"

---
+++ HackerRank/Easy/SimpleArraySum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Easy
+{
+    public class SimpleArraySum
+    {
+
+        public static int SolutionA(List<int> ar)
+        {
+            return ar.Sum();
+        }
+
+    }
+}
