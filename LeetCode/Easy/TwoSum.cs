43a321c593879336cb1af026a9a6dd405d5c6075
Mario Pinzón <mariopinzon.dev@gmail.com>
2025-08-24 11:51:52 -0500

    exercise(leetcode): solve "Two Sum"

---
+++ LeetCode/Easy/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class TwoSum
+    {
+
+        public static int[] SolutionA(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+
+    }
+}
