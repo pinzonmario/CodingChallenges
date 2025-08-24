ae47718b87128ff10059e946304938733cd9b31b
Mario Pinzón <mariopinzon.dev@gmail.com>
2025-08-24 11:03:32 -0500

    exercise(codewars): complete kata "Descending Order"

---
+++ CodeWars/7Kyu/DescendingOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._7Kyu
+{
+    public class DescendingOrder
+    {
+        public static int SolutionA(int num)
+        {
+            return int.Parse(string.Join("", num.ToString().ToList().OrderByDescending(x => x)));
+        }
+    }
+}
