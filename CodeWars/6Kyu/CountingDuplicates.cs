34583fd4ee40183a9305790d595bc11b6a415e9c
pinzonmario <mariopinzon.dev@gmail.com>
2025-11-02 13:41:24 -0500

    exercise(CodeWars): solve "Counting Duplicates"

---
+++ CodeWars/6Kyu/CountingDuplicates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._6Kyu
+{
+    public class CountingDuplicates
+    {
+        public static int SolutionA(string str)
+        {
+            var text = str.ToLower();
+
+            var duplicates = text.GroupBy(x => x)
+                                 .Where(x => x.Count() > 1)
+                                 .Select(x => x.Key);
+
+            return duplicates.Count();
+        }
+    }
+}
