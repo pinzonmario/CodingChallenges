8b7daf91055f53731b0a660563c4df94bd228d60
Mario Pinzón <mariopinzon.dev@gmail.com>
2026-01-24 11:13:38 -0500

    feat(leetcode): solve #9 Palindrome Number

---
+++ LeetCode/Easy/PalindromeNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class PalindromeNumber
+    {
+        public static bool Solution(int x)
+        {
+            string value = x.ToString();
+            return value.SequenceEqual(value.Reverse());
+        }
+    }
+}
