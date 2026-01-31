5fa0551bb25540ecaabcc74db9514b40dbdf9f06
Mario Pinzón <mariopinzon.dev@gmail.com>
2026-01-31 11:54:51 -0500

    feat(hackerrank): solve "Diagonal Difference"

---
+++ HackerRank/Easy/DiagonalDifference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Easy
+{
+    public class DiagonalDifference
+    {
+        public static int Solution(List<List<int>> arr)
+        {
+            int d1 = 0;
+            int d2 = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = i; j <= i; j++)
+                {
+                    d1 += arr[i][j];
+                    d2 += arr[i][arr.Count - 1 - i];
+                }
+            }
+
+            return Math.Abs(d1 - d2);
+        }
+    }
+}
